3375652c77c70c7ab2f30824cf815566e3b51598
cbhutad <learningcbhutad@gmail.com>
2023-09-23 12:09:38 +0530

    Constants to represent strings Loinc.csv, PanelsandForms.csv and Part.csv

---
+++ loinc-lib/src/main/java/in/cdac/medinfo/loinc/Constants.java
@@ -0,0 +1,7 @@
+package in.cdac.medinfo.loinc
+
+public class Constants {
+	public static final String LOINC = "Loinc.csv";
+	public static final String PANEL_AND_FORMS = "PanelsAndForms.csv";
+	public static final String PART = "Part.csv";
+}
