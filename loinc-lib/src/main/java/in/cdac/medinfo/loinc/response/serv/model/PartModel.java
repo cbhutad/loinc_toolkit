a32315614dbe3d8fb7aaa28b76cff35968102507
cbhutad <learningcbhutad@gmail.com>
2023-10-12 00:05:00 +0530

    Added PartModel pojo

---
+++ loinc-lib/src/main/java/in/cdac/medinfo/loinc/response/serv/model/PartModel.java
@@ -0,0 +1,61 @@
+package in.cdac.medinfo.loinc.response.serv.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/*
+ * 
+ * This class consists of all resposne parameters for all supporting apis.
+ * 
+ */
+
+
+@JsonPropertyOrder({"LOINC_PART_NUMBER", "LOINC_PART_NAME", "LOINC_PART_DESCRIPTION", "STATUS"})
+public class PartModel {
+
+    private String LOINC_PART_NUMBER;
+    private String LOINC_PART_NAME;
+    private String LOINC_PART_DESCRIPTION;
+    private String STATUS;
+
+    @JsonProperty("LOINC_PART_NUMBER")
+    private String getLOINC_PART_NUMBER() {
+        return this.LOINC_PART_NUMBER;
+    }
+
+    @JsonProperty("LOINC_PART_NUMBER")
+    private void setLOINC_PART_NUMBER(String loinc_PART_NUMBER) {
+        this.LOINC_PART_NUMBER = loinc_PART_NUMBER;
+    }
+
+    @JsonProperty("LOINC_PART_NAME")
+    private String getLOINC_PART_NAME() {
+        return this.LOINC_PART_NAME;
+    }
+
+    @JsonProperty("LOINC_PART_NAME")
+    private void setLOINC_PART_NAME(String LOINC_PART_NAME) {
+        this.LOINC_PART_NAME = LOINC_PART_NAME;
+    }
+
+    @JsonProperty("LOINC_PART_DESCRIPTION")
+    private String getLOINC_PART_DESCRIPTION() {
+        return this.LOINC_PART_DESCRIPTION;
+    }
+
+    @JsonProperty("LOINC_PART_DESCRIPTION")
+    private void setLOINC_PART_DESCRIPTION(String LOINC_PART_DESCRIPTION) {
+        this.LOINC_PART_DESCRIPTION = LOINC_PART_DESCRIPTION;
+    }
+
+    @JsonProperty("STATUS")
+    private String getSTATUS() {
+        return this.STATUS;
+    }
+
+    @JsonProperty("STATUS")
+    private void setSTATUS(String STATUS) {
+        this.STATUS = STATUS;
+    }
+
+}
