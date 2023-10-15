b1c8f6ff17239b168a4cdaed5dd462325a75f6a4
cbhutad <learningcbhutad@gmail.com>
2023-10-15 19:37:50 +0530

    Change Modifiers of getters-setters to public from private

--- loinc-lib/src/main/java/in/cdac/medinfo/loinc/response/serv/model/PartModel.java
+++ loinc-lib/src/main/java/in/cdac/medinfo/loinc/response/serv/model/PartModel.java
@@ -19,42 +19,42 @@ public class PartModel {
     private String STATUS;
 
     @JsonProperty("LOINC_PART_NUMBER")
-    private String getLOINC_PART_NUMBER() {
+    public String getLOINC_PART_NUMBER() {
         return this.LOINC_PART_NUMBER;
     }
 
     @JsonProperty("LOINC_PART_NUMBER")
-    private void setLOINC_PART_NUMBER(String loinc_PART_NUMBER) {
+    public void setLOINC_PART_NUMBER(String loinc_PART_NUMBER) {
         this.LOINC_PART_NUMBER = loinc_PART_NUMBER;
     }
 
     @JsonProperty("LOINC_PART_NAME")
-    private String getLOINC_PART_NAME() {
+    public String getLOINC_PART_NAME() {
         return this.LOINC_PART_NAME;
     }
 
     @JsonProperty("LOINC_PART_NAME")
-    private void setLOINC_PART_NAME(String LOINC_PART_NAME) {
+    public void setLOINC_PART_NAME(String LOINC_PART_NAME) {
         this.LOINC_PART_NAME = LOINC_PART_NAME;
     }
 
     @JsonProperty("LOINC_PART_DESCRIPTION")
-    private String getLOINC_PART_DESCRIPTION() {
+    public String getLOINC_PART_DESCRIPTION() {
         return this.LOINC_PART_DESCRIPTION;
     }
 
     @JsonProperty("LOINC_PART_DESCRIPTION")
-    private void setLOINC_PART_DESCRIPTION(String LOINC_PART_DESCRIPTION) {
+    public void setLOINC_PART_DESCRIPTION(String LOINC_PART_DESCRIPTION) {
         this.LOINC_PART_DESCRIPTION = LOINC_PART_DESCRIPTION;
     }
 
     @JsonProperty("STATUS")
-    private String getSTATUS() {
+    public String getSTATUS() {
         return this.STATUS;
     }
 
     @JsonProperty("STATUS")
-    private void setSTATUS(String STATUS) {
+    public void setSTATUS(String STATUS) {
         this.STATUS = STATUS;
     }
 
