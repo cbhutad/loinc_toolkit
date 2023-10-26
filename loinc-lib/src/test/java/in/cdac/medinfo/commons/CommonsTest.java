2e1056fd9c496aa2839568cde332cc9f36ecfa71
cbhutad <learningcbhutad@gmail.com>
2023-10-26 23:06:29 +0530

    Relocated CommonsTest.java

---
+++ loinc-lib/src/test/java/in/cdac/medinfo/commons/CommonsTest.java
@@ -0,0 +1,37 @@
+package in.cdac.medinfo.commons;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import in.cdac.medinfo.loinc.commons.*;
+
+public class CommonsTest {
+
+	private final StringToClassTypesConverter classTypeConverter = new StringToClassTypesConverter();	
+	private final StringToPanelTypesConverter panelTypeConverter = new StringToPanelTypesConverter();
+	private final StringToStatusConverter statusConverter = new StringToStatusConverter();
+
+	@Test
+	void classTypeConversionTest() {
+		assertEquals(EnumClassTypes.LABORATORY, classTypeConverter.convert("laboratory"));
+		assertEquals(EnumClassTypes.CLINICAL, classTypeConverter.convert("clinical"));
+		assertEquals(EnumClassTypes.SURVEY, classTypeConverter.convert("survey"));
+		assertEquals(EnumClassTypes.ATTACHMENT, classTypeConverter.convert("attachment"));
+		assertEquals(EnumClassTypes.ALL, classTypeConverter.convert("all"));
+	}
+	@Test
+	void panelTypeConversionTest() {
+		assertEquals(EnumPanelTypes.PANEL, panelTypeConverter.convert("panel"));
+		assertEquals(EnumPanelTypes.ORGANIZER, panelTypeConverter.convert("organizer"));
+		assertEquals(EnumPanelTypes.CONVENIENCE_GROUP, panelTypeConverter.convert("convenience_group"));
+		assertEquals(EnumPanelTypes.ALL, panelTypeConverter.convert("all"));
+	}
+	@Test
+	void statusConversionTest() {
+		assertEquals(EnumStatus.ACTIVE, statusConverter.convert("active"));
+		assertEquals(EnumStatus.TRIAL, statusConverter.convert("trial"));
+		assertEquals(EnumStatus.DISCOURAGED, statusConverter.convert("discouraged"));
+		assertEquals(EnumStatus.DEPRECATED, statusConverter.convert("deprecated"));
+		assertEquals(EnumStatus.ALL, statusConverter.convert("all"));
+	}
+
+}
