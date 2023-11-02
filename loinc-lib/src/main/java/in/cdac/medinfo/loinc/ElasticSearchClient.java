eabfa248142a24ceaecb1e891040578951bb95c0
cbhutad <learningcbhutad@gmail.com>
2023-11-02 23:01:08 +0530

    LookUp API logic added

--- loinc-lib/src/main/java/in/cdac/medinfo/loinc/ElasticSearchClient.java
+++ loinc-lib/src/main/java/in/cdac/medinfo/loinc/ElasticSearchClient.java
@@ -4,13 +4,16 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
+import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
 import co.elastic.clients.elasticsearch._types.aggregations.TermsAggregation;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.MatchPhrasePrefixQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermQuery;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.Hit;
@@ -21,6 +24,8 @@ import in.cdac.medinfo.loinc.exceptions.CodeNotFoundException;
 import in.cdac.medinfo.loinc.exceptions.InternalServerException;
 import in.cdac.medinfo.loinc.response.serv.model.PanelModel;
 import in.cdac.medinfo.loinc.response.serv.model.PartModel;
+import in.cdac.medinfo.loinc.response.serv.model.ChildModel;
+import in.cdac.medinfo.loinc.response.serv.model.Model;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,6 +40,164 @@ import org.apache.logging.log4j.LogManager;
 public class ElasticSearchClient {
 
     private static final Logger logger = LogManager.getLogger(ElasticSearchClient.class);
+
+
+    /**
+     * @param loincNum
+     * @return List<Model>
+     */
+
+    public Model lookup(String loincNum) {
+
+        Model concept;
+
+        try {
+            
+            //Parent Loinc Concept Query code
+            TermQuery termQuery = new TermQuery.Builder().field("LOINC_NUM.keyword").value(FieldValue.of(loincNum)).build();
+            Query query = new Query.Builder().term(termQuery).build();
+            SearchRequest searchRequest = new SearchRequest.Builder().index("loinc").query(query).build();
+            SearchResponse<Object> searchResponse = ElasticSearchConfiguration.elasticsearchClient.search(searchRequest, Object.class); 
+
+            List<Hit<Object>> hit = searchResponse.hits().hits();
+            TotalHits totalHits = searchResponse.hits().total();
+            long count = totalHits.value();
+
+            if(count == 0) {
+                throw new NoSuchElementException("ERROR :  code " + loincNum + " not found");
+            }
+
+            concept = new Model();
+            Object source = hit.get(0).source();
+
+            @SuppressWarnings("unchecked")
+            Map<String, Object> sourceAsMap = (Map<String, Object>) source;
+
+            concept.setCLASS((String) sourceAsMap.get("CLASS"));
+
+            String classType = (String) sourceAsMap.get("CLASSTYPE");
+            if(classType.compareToIgnoreCase("1") == 0) {
+                concept.setCLASSTYPE("Laboratory");
+            } else if(classType.compareToIgnoreCase("2") == 0) {
+                concept.setCLASSTYPE("Clinical");
+            } else if(classType.compareToIgnoreCase("3") == 0) {
+                concept.setCLASSTYPE("Attachment");
+            } else {
+                concept.setCLASSTYPE("Survey");
+            }
+
+            concept.setCOMPONENT((String) sourceAsMap.get("COMPONENT"));
+            concept.setDisplayName((String) sourceAsMap.get("DisplayName"));
+            concept.setExampleUnits((String) sourceAsMap.get("EXAMPLE_UCUM_UNITS"));
+            concept.setLOINC((String) sourceAsMap.get("LOINC_NUM"));
+            concept.setLONG_COMMON_NAME((String) sourceAsMap.get("LONG_COMMON_NAME"));
+            concept.setMETHOD_TYP((String) sourceAsMap.get("METHOD_TYP"));
+            concept.setORDER_OBS((String) sourceAsMap.get("ORDER_OBS"));
+            concept.setPROPERTY((String) sourceAsMap.get("PROPERTY"));
+            concept.setRelatedNames((String) sourceAsMap.get("RELATEDNAMES2"));
+            concept.setSCALE_TYP((String) sourceAsMap.get("SCALE_TYP"));
+            concept.setSTATUS((String) sourceAsMap.get("STATUS"));
+            concept.setSYSTEM((String) sourceAsMap.get("SYSTEM"));
+            concept.setShortName((String) sourceAsMap.get("SHORTNAME"));
+            concept.setTIME_ASPCT((String) sourceAsMap.get("TIME_ASPCT"));
+            concept.setVersionFirstReleased((String) sourceAsMap.get("VersionFirstReleased"));
+            concept.setVersionLastChanged((String) sourceAsMap.get("VersionLastChanged"));
+
+            //Searching for child concepts if any
+            MatchPhrasePrefixQuery matchPhrasePrefixQuery = new MatchPhrasePrefixQuery.Builder().field("ParentLoinc").query(loincNum).build();
+            Query childConceptsQuery = new Query.Builder().matchPhrasePrefix(matchPhrasePrefixQuery).build();
+            SearchRequest childConceptSearchRequest = new SearchRequest.Builder().index("loincPanel").query(childConceptsQuery).from(0).size(10000).build();
+            SearchResponse<Object> childConceptSearchResponse = ElasticSearchConfiguration.elasticsearchClient.search(childConceptSearchRequest, Object.class);
+
+            List<Hit<Object>> childHits = childConceptSearchResponse.hits().hits();
+            TotalHits childTotalHits = childConceptSearchResponse.hits().total();
+
+            List<ChildModel> childList = new ArrayList<ChildModel>();
+
+            if(childTotalHits.value() != 0) {
+
+                String parentId = "";
+
+                for(Hit<Object> hitPanel : childHits) {
+
+                    ChildModel childConcept = new ChildModel();
+                    Object childSource = hitPanel.source();
+
+                    @SuppressWarnings("unchecked")
+                    Map<String, Object> childSourceAsMap = (Map<String, Object>) childSource;
+
+                    String parentIdCompare = parentId;
+                    parentId = (String) childSourceAsMap.get("ParentId");
+
+                    if(parentIdCompare.compareToIgnoreCase(parentId) != 0 && parentIdCompare.compareToIgnoreCase("") != 0)
+                        break;
+
+                    String loincChild = (String) childSourceAsMap.get("Loinc");
+                    if(loincChild.compareToIgnoreCase(loincNum) != 0) {
+
+                        MatchPhrasePrefixQuery matchPhrasePrefixQueryGrandChildren = new MatchPhrasePrefixQuery.Builder().field("ParentLoinc").query(loincChild).build();
+                        Query grandChildrenQuery = new Query.Builder().matchPhrasePrefix(matchPhrasePrefixQueryGrandChildren).build();
+                        SearchRequest grandChildrenSearchRequest = new SearchRequest.Builder().index("loincPanel").query(grandChildrenQuery).from(0).size(10000).build();
+                        SearchResponse<Object> grandChildrenSearchResponse = ElasticSearchConfiguration.elasticsearchClient.search(grandChildrenSearchRequest, Object.class);
+
+                        List<Hit<Object>> grandChildHits = grandChildrenSearchResponse.hits().hits();
+                        TotalHits grandChildTotalHits = grandChildrenSearchResponse.hits().total();
+                        long grandChildrenCount = grandChildTotalHits.value();
+
+                        if (grandChildrenCount != 0) {
+                            List<ChildModel> grandChildList = new ArrayList<>();
+                            
+                            for(Hit<Object> grandChild : grandChildHits) {
+
+                                ChildModel grandChildConcept = new ChildModel();
+                                Object grandChildSource = grandChild.source();
+
+                                @SuppressWarnings("unchecked")
+                                Map<String,Object> grandChildSourceAsMap = (Map<String, Object>) grandChildSource;
+
+                                String grandChildLoinc = (String) grandChildSourceAsMap.get("Loinc");
+                                if(loincChild.compareToIgnoreCase(grandChildLoinc) != 0) {
+                                    grandChildConcept.setLOINC(grandChildLoinc);
+                                    grandChildConcept.setExample_UCUM_UNITS((String)
+                                    grandChildSourceAsMap.get("EXAMPLE_UCUM_UNITS"));
+                                    grandChildConcept.setName((String) grandChildSourceAsMap.get("SHORTNAME"));
+                                    grandChildConcept.setObservationRequiredInPanel((String) grandChildSourceAsMap.get("ObservationRequiredInPanel"));
+                                    grandChildConcept.setGrandChildren(new ArrayList<>());
+
+                                    grandChildList.add(grandChildConcept);
+                                }
+                            }
+                            childConcept.setGrandChildren(grandChildList);
+
+                        }
+
+                        childConcept.setExample_UCUM_UNITS((String) childSourceAsMap.get("EXAMPLE_UCUM_UNITS"));
+                        childConcept.setLOINC(loincChild);
+                        childConcept.setName((String) childSourceAsMap.get("SHORTNAME"));
+                        childConcept.setObservationRequiredInPanel((String) childSourceAsMap.get("ObservationRequiredInPanel"));
+
+                        childList.add(childConcept);
+                    }
+
+
+                }
+
+                concept.setChildren(childList);
+            }
+            
+        } catch (NoSuchElementException ex) {
+            logger.error(ex.getMessage());
+            throw new CodeNotFoundException(ex.getMessage());
+        } catch (ElasticsearchException ex) {
+            logger.error("ERROR : " + ex.getMessage() + " Please check your connection with Elasticsearch");
+            throw new InternalServerException("ERROR : " + ex.getMessage() + " Please check your connection with Elasticsearch");
+        } catch (Exception ex) {
+            logger.error("ERROR : " + ex.getMessage());
+            throw new InternalServerException("ERROR : " + ex.getMessage());
+        }
+
+        return concept;
+    }
 
     /**
      * 
