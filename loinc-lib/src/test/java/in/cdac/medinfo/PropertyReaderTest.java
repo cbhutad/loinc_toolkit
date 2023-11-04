eda774d934fe3e1dc236860714b5616aab9b4370
cbhutad <learningcbhutad@gmail.com>
2023-11-04 12:34:42 +0530

    Test added to check InternalServerException thrown

--- loinc-lib/src/test/java/in/cdac/medinfo/PropertyReaderTest.java
+++ loinc-lib/src/test/java/in/cdac/medinfo/PropertyReaderTest.java
@@ -1,8 +1,11 @@
 package in.cdac.medinfo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import in.cdac.medinfo.loinc.PropertyReader;
+import in.cdac.medinfo.loinc.exceptions.InternalServerException;
 
 public class PropertyReaderTest {
 
@@ -19,5 +22,16 @@ public class PropertyReaderTest {
         assertTrue(PropertyReader.elasticsearchPort == Integer.parseInt(port));
         assertTrue(PropertyReader.loincFolderPath.equals(loincFolderPath));
 
+    }
+
+    @Test
+    void propertyReadFileLogTest() {
+        String loincFolderPath = "/home/cbhutad/Work/loinc_toolkit/logs/loinc_lib";
+        String elasticsearchHost = "http://localhost";
+        String port = "Nine thousand two hundread";
+
+        PropertyReader propertyReader = new PropertyReader();
+        Exception ex = assertThrows(InternalServerException.class, () -> propertyReader.propertyRead(loincFolderPath, elasticsearchHost, port));
+        assertEquals("ERROR :For input string: \"Nine thousand two hundread\" Please enter correct Configuration details", ex.getMessage());
     }
 }
